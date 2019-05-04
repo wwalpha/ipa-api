00b25f6f5280f845ade7e650c3147954c608c463
wwalpha <wwalpha@gmail.com>
2019-05-04 20:23:29 +0900

    test

--- cdk/README.md
+++
@@ -1,6 +0,0 @@
-## Install
-```
-npm i -g aws-cdk
-npm install
-npm run dp
-```
--- cdk/src/app.ts
+++
@@ -1,5 +0,0 @@
-import cdk = require('@aws-cdk/cdk');
-import { CdkStack } from './stack';
-
-const app = new cdk.App();
-new CdkStack(app, 'ipa-api');
