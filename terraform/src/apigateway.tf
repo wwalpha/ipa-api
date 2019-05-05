be8556cd438b7ed4a692a1d33e96fee3f600029c
wwalpha <wwalpha@gmail.com>
2019-05-05 17:51:05 +0900

    update

--- terraform/src/apigateway.tf
+++ terraform/src/apigateway.tf
@@ -17,14 +17,47 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   stage_name  = "v1"
 }
 
+# --------------------------------------------------------------------------------
+# Amazon API Gateway Usage Plan
+# --------------------------------------------------------------------------------
+resource "aws_api_gateway_usage_plan" "usage_plan" {
+  name = "ipa_usage_plan"
+
+  api_stages {
+    api_id = "${aws_api_gateway_rest_api.ipa_api.id}"
+    stage  = "${aws_api_gateway_deployment.api_deployment.stage_name}"
+  }
+
+  quota_settings {
+    limit  = 200
+    period = "DAY"
+  }
+
+  throttle_settings {
+    burst_limit = 5
+    rate_limit  = 5
+  }
+}
+
+resource "aws_api_gateway_api_key" "api_key" {
+  name = "ipa_api_key"
+}
+
+resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
+  key_id        = "${aws_api_gateway_api_key.api_key.id}"
+  key_type      = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.usage_plan.id}"
+}
+
 module "ipa" {
   # source        = "github.com/wwalpha/terraform-modules-api-lambda"
-  source        = "./terraform-modules-api-lambda"
-  region        = "${var.region}"
-  rest_api_id   = "${aws_api_gateway_rest_api.ipa_api.id}"
-  http_method   = "GET"
-  resource_id   = "${aws_api_gateway_rest_api.ipa_api.root_resource_id}"
-  authorization = "NONE"
+  source           = "./terraform-modules-api-lambda"
+  region           = "${var.region}"
+  rest_api_id      = "${aws_api_gateway_rest_api.ipa_api.id}"
+  http_method      = "GET"
+  resource_id      = "${aws_api_gateway_rest_api.ipa_api.root_resource_id}"
+  authorization    = "NONE"
+  api_key_required = true
 
   request_parameters = {
     "method.request.querystring.word" = true
